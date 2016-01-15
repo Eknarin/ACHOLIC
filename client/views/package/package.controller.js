879a6261caf58e0c6420afc96d6b2b85be37ee7e
Natcha119 <natchachid@gmail.com>
2016-01-15 12:33:57 +0700

    Rotate filter button when click

--- client/views/package/package.controller.js
+++ client/views/package/package.controller.js
@@ -19,5 +19,19 @@ angular.module('acholic')
     $scope.getNumber = function(num) {
 	    return new Array(num);   
 	};
+	$scope.deg = 180;
+	$scope.rotate = function(deg){
+		$('#expandButt').css({
+        '-webkit-transform': 'rotate(' + deg + 'deg)',  //Safari 3.1+, Chrome  
+        '-moz-transform': 'rotate(' + deg + 'deg)',     //Firefox 3.5-15  
+        '-ms-transform': 'rotate(' + deg + 'deg)',      //IE9+  
+        '-o-transform': 'rotate(' + deg + 'deg)',       //Opera 10.5-12.00  
+        'transform': 'rotate(' + deg + 'deg)'          //Firefox 16+, Opera 12.50+  
+    	});
+    	$scope.deg += 180;
+    	if($scope.deg == 360){
+    		$scope.deg = 0;
+    	}
+	};
 
   }]);
