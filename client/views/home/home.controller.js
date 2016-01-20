5ac36902a7cbe97e87dc0b0fc24984f6ea9205d8
Eknarin <oat-et@hotmail.com>
2016-01-20 21:04:47 +0700

    Start implement Recommended Packages in Home Page

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -1,25 +1,14 @@
 'use strict';
-
-// angular.module('acholic')
-//   .controller('HomeCtrl', function () {
-
-//     var vm = this;
-
-//     angular.extend(vm, {
-//       name: 'HomeCtrl'
-//     });
-//   });
-
- angular.module('acholic')
-	  .controller('HomeCtrl',['$scope', function ($scope){
-	  $scope.sliderpics = [
-		  {
+angular.module('acholic')
+	.controller('HomeCtrl',['$scope', function ($scope){
+	$scope.sliderpics = [
+		{
 			name: 'A',
 			src:'../image/TEST.png'  
-		  },	 
-		  {
+		},	 
+		{
 		  	name: 'B',
 		  	src:'../image/TEST2.png'
-		  }
-	  ]
-  }]);
+		}
+	]
+}]);
