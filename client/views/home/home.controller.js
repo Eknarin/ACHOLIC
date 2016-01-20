8cf13b30b900446f000c00f518c451d02fa2bbf2
Eknarin <oat-et@hotmail.com>
2016-01-20 18:20:59 +0700

    - Edit slider homepage - Remove app.css in .gitignore

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -1,11 +1,25 @@
 'use strict';
 
-angular.module('acholic')
-  .controller('HomeCtrl', function () {
+// angular.module('acholic')
+//   .controller('HomeCtrl', function () {
 
-    var vm = this;
+//     var vm = this;
 
-    angular.extend(vm, {
-      name: 'HomeCtrl'
-    });
-  });
+//     angular.extend(vm, {
+//       name: 'HomeCtrl'
+//     });
+//   });
+
+ angular.module('acholic')
+	  .controller('HomeCtrl',['$scope', function ($scope){
+	  $scope.sliderpics = [
+		  {
+			name: 'A',
+			src:'../image/TEST.png'  
+		  },	 
+		  {
+		  	name: 'B',
+		  	src:'../image/TEST2.png'
+		  }
+	  ]
+  }]);
