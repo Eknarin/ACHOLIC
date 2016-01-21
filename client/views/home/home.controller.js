dac5ac98167c221e4beae836274498005ca7c988
Eknarin <oat-et@hotmail.com>
2016-01-21 14:20:20 +0700

    Create Recommended Packages in Home Page

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -11,4 +11,48 @@ angular.module('acholic')
 		  	src:'../image/TEST2.png'
 		}
 	]
+
+	$scope.recommendedPackages = [
+		{
+			name: '1',
+			src:'../image/TEST.png'
+		},
+
+		{
+			name: '2',
+			src:'../image/TEST2.png'
+		},
+
+		{
+			name: '3'
+		},
+
+		{
+			name: '4'
+		},
+
+		{
+			name: '5'
+		},
+
+		{
+			name: '6'
+		},
+
+		{
+			name: '7'
+		},
+
+		{
+			name: '8'
+		},
+
+		{
+			name: '9'
+		},
+
+		{
+			name: '10'
+		}
+	]
 }]);
