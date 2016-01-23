944b80c44da38b15d85768b797f7aa30ce36773c
Kridpak <Mac_save@Mores-mini.local>
2016-01-23 12:27:17 +0700

    Add package detail , add seed data

--- server/migrations/data.js
+++ server/migrations/data.js
@@ -6,41 +6,108 @@ var data = [
                 "name": "Package1",
                 "description": "Des pack 1",
                 "rating": "8",
-                "price": "0",
+                "price": "3999",
                 "contact": "-",
-                "office_time": "7:00-24:00"
+                "office_time": 
+                    {
+                        "open_time": "7:00 AM",
+                        "close_time": "11:30 PM"
+                    }
+                ,
+                "contact": 
+                    {
+                        "email": "test1@homtail.com",
+                        "phone_number": "081-111-1111"
+                    }
+                ,
+                "people": "10-15",
+                "availability": "true"
+                
             },
             {
                 "name": "Package2",
                 "description": "Des pack 2",
                 "rating": "10",
-                "price": "0",
+                "price": "490",
                 "contact": "-",
-                "office_time": "7:00-24:00"
+                "office_time": 
+                    {
+                        "open_time": "7:00 AM",
+                        "close_time": "11:30 PM"
+                    }
+                ,
+                "contact": 
+                    {
+                        "email": "test2@homtail.com",
+                        "phone_number": "081-111-1112"
+                    }
+                ,
+                "people": "1-5",
+                "availability": "true"
+
             },
             {
                 "name": "Package3",
                 "description": "Des pack 3",
                 "rating": "9",
-                "price": "0",
+                "price": "1999",
                 "contact": "-",
-                "office_time": "7:00-24:00"
+                "office_time": 
+                    {
+                        "open_time": "7:00 AM",
+                        "close_time": "11:30 PM"
+                    }
+                ,
+                "contact": 
+                    {
+                        "email": "test3@homtail.com",
+                        "phone_number": "081-111-1113"
+                    }
+                ,
+                "people": "20-25",
+                "availability": "true"
             },
             {
                 "name": "Package4",
                 "description": "Des pack 4",
                 "rating": "10",
-                "price": "0",
+                "price": "9999",
                 "contact": "-",
-                "office_time": "7:00-24:00"
+                "office_time": 
+                    {
+                        "open_time": "7:00 AM",
+                        "close_time": "11:30 PM"
+                    }
+                ,
+                "contact": 
+                    {
+                        "email": "test4@homtail.com",
+                        "phone_number": "081-111-1114"
+                    } 
+                ,
+                "people": "30+",
+                "availability": "false"           
             },
             {
                 "name": "Package5",
                 "description": "Des pack 4",
                 "rating": "6",
-                "price": "0",
+                "price": "199",
                 "contact": "-",
-                "office_time": "7:00-24:00"
+                "office_time": 
+                    {
+                        "open_time": "7:00 AM",
+                        "close_time": "11:30 PM"
+                    }
+                ,
+                "contact": 
+                    {
+                        "email": "test5@homtail.com",
+                        "phone_number": "081-111-1115"
+                    }
+                ,
+                "people": "5-10",
+                "availability": "true"
             }
         ]
     },
