35489a927c156dc647e33c690213f5109df77f5b
Kridpak <s.vorametpasuk@gmail.com>
2016-01-24 18:34:21 +0700

    Fig bug show image

--- client/views/package-create/package-create.controller.js
+++ client/views/package-create/package-create.controller.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('acholic')
-  .controller('PackageCreateCtrl',['$scope','PackageItem',function ($scope , PackageItem) {
+  .controller('PackageCreateCtrl',['$scope','PackageItem','$location',function ($scope , PackageItem , $location) {
 
   	$scope.packageItem = new PackageItem;
-
+  
   	$scope.onSubmit = function(){
-  		console.log($scope.packageItem);
-  		$scope.packageItem.$save();
+  		//console.log($scope.packageItem);
+  		$scope.packageItem.$save().then(function(){
+  			 $location.path("/package");
+  		});
   	};
 
   }]);
