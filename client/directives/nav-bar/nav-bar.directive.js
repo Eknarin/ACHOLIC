1a527790538fdc06ec88a8e1db581d2317243ba7
Natcha119 <natchachid@gmail.com>
2016-02-16 20:58:38 +0700

    Make sign in modal with Auth controller.

--- client/directives/nav-bar/nav-bar.directive.js
+++ client/directives/nav-bar/nav-bar.directive.js
@@ -1,19 +1,34 @@
 'use strict';
-
-angular.module('acholic')
-  .directive('navBar', function () {
+var myNav = angular.module('acholic');
+  myNav.directive('navBar', function () {
     return {
       restrict: 'E',
       templateUrl: 'directives/nav-bar/nav-bar.html',
-      controller: ['$scope' , '$location', '$route' ,'Auth' ,function($scope , $location , $route , Auth) {
-      	$scope.search = '';
+      controller: ['$scope' , '$location', '$route' ,'Auth' ,function($scope , $location , $route ,$uibModal, Auth) {
+
+        $scope.search = '';
 
       	$scope.searchPage = function(){
           var temp = $scope.search;
           $scope.search = '';
       		$location.path("/package").search({q: temp});
       	};
-      }]
-	    
+    
+      }],      
     };
   });
+  myNav.controller('ItemController', ['$scope','Auth',function ($scope, Auth) {
+    var vm = this;
+
+     $scope.user = { email: 'test@test.com', password: 'test' };
+
+     $scope.login = function () {
+          Auth.login($scope.user)
+            .then(function () {
+              $('#signin-modal').modal('hide');
+            })
+            .catch(function (err) {
+              vm.error = err;
+            });
+        };
+  }]);
