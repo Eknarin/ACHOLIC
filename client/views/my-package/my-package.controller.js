4f419f5edadde7e60699a8e788a35bcc0a87d934
Kridpak <s.vorametpasuk@gmail.com>
2016-03-05 22:23:51 +0700

    Fix bug loading my bookmark and package

--- client/views/my-package/my-package.controller.js
+++ client/views/my-package/my-package.controller.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('acholic')
-  .controller('MyPackageCtrl',['$scope','PackageItem','$rootScope', function ($scope, PackageItem,$rootScope) {
-  	$scope.user = $rootScope._user;
+  .controller('MyPackageCtrl',['$scope','PackageItem','Auth', function ($scope, PackageItem,Auth) {
+  	$scope.user = {};
   	$scope.package = {};
-  	PackageItem.myPackage({q: $scope.user._id , page: 1}).$promise.then(function(res){
-  		$scope.package = res.docs;
-  		console.log($scope.package);
+  	Auth.getUser().then(function(res){
+  		$scope.user = res;
+  		PackageItem.myPackage({q: $scope.user._id , page: 1}).$promise.then(function(res){
+	  		$scope.package = res;
+	  		console.log($scope.package);
+  		});
   	});
+
 
   }]);
