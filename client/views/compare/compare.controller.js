f26c32c9f719bf14a9295f2d789678919a171ebb
Eknarin <oat-et@hotmail.com>
2016-03-08 14:51:00 +0700

    Packages are able to compare

--- client/views/compare/compare.controller.js
+++ client/views/compare/compare.controller.js
@@ -10,6 +10,35 @@ angular.module('acholic')
   		$scope.comparePackage.items = res;
   		$scope.loadItem = true;
 		console.log($scope.comparePackage);
-  	});
+  	});  
+
+  	$scope.getStar = function(num) {
+    if(num == null){
+      $scope.rate = 0;
+    }
+    else{
+      $scope.rate = num/2;
+    }
+        
+	    return new Array(Math.floor($scope.rate));   
+	};
+	
+	$scope.getWhite = function(val){
+    if(val == null){
+      $scope.yStar = 0;
+    }
+    else{
+      if((val/2)%1 == 0){
+        $scope.yStar = Math.floor(val/2);
+      } else{
+        if(((val)/2)%1 >= 0.5){
+          $scope.yStar = Math.floor(val/2)+1;
+        }else{
+          $scope.yStar = Math.floor(val/2);
+        }
+      }
+    }
+    return new Array(5-$scope.yStar);
+  };	
 
   }]);
