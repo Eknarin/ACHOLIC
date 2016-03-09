e2247fbdf2e8b60cc502b275c803850f1dc5458c
Kridpak <s.vorametpasuk@gmail.com>
2016-03-09 20:17:57 +0700

    Add resource to bookmark

--- client/services/bookmark/bookmark.resource.js
+++ client/services/bookmark/bookmark.resource.js
@@ -9,6 +9,15 @@ angular.module('acholic')
       save: {
       	method: 'POST'
       },
+      saveFolder:{
+        method: 'POST',
+        url: 'api/bookmarks/folder'
+      },
+      queryFolder:{
+        method: 'GET',
+        url: 'api/bookmarks/folder',
+        isArray: true
+      },
       query:{
         method: 'GET',
         isArray: true
