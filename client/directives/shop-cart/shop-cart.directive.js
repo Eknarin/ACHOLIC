c7bde7f18a15cd26f5f602c865f25eaf777478ad
Natcha119 <natchachid@gmail.com>
2016-03-12 22:33:08 +0700

    Show cart modal when click cart button.

--- client/directives/shop-cart/shop-cart.directive.js
+++ client/directives/shop-cart/shop-cart.directive.js
@@ -5,13 +5,35 @@ angular.module('acholic')
     return {
       restrict: 'EA',
       templateUrl: 'directives/shop-cart/shop-cart.html',
-      controller: ['$scope' , '$location','$uibModal' ,'$rootScope','Cart',function($scope , $location ,$uibModal ,$rootScope,Cart ) {
-      	$scope.cart = Cart.getCart();
-        // console.log($scope.cart);
+      controller: ['$scope' , '$location','$uibModal', 'Auth','$rootScope','Cart',function($scope , $location ,$uibModal, Auth,$rootScope,Cart ) {
+      	$scope.cart = [];
 
-        $scope.openModal = function(){
-          console.log("open cart");
+        Auth.getUser().then(function(res){
+          $scope.userId = res._id;
+          var temp_cart = Cart.getCart($scope.userId);
+          $scope.getItemCart(temp_cart);
+        });
+
+        $scope.getItemCart = function(carts){
+          for(var i=0; i<carts.length ;i++){
+            for(var j=0; j<carts[i].list.length ;j++){
+              var item = {
+                package: carts[i].package,
+                cart_item: carts[i].list[j]
+              }
+              $scope.total_price += Number(carts[i].list[j].price);
+              $scope.cart.push(item);
+            }
+          }
           console.log($scope.cart);
+        };
+
+        $scope.viewCart = function(){
+          console.log("Show cart");
+        };
+
+        $scope.checkout = function(){
+          console.log("Checkout this cart");
         };
 
       }],
