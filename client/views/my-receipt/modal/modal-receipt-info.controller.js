1bde7f021334f36b58b50891981b72424903887e
Natcha119 <natchachid@gmail.com>
2016-04-17 16:07:39 +0700

    Add package name to receipt.

--- client/views/my-receipt/modal/modal-receipt-info.controller.js
+++ client/views/my-receipt/modal/modal-receipt-info.controller.js
@@ -1,19 +1,30 @@
 'use strict';
 
 angular.module('acholic')
-  .controller('ReceiptInfoModalCtrl',['$scope','$uibModalInstance', '$location','itemData', function ($scope, $uibModalInstance, $location,itemData) {
+  .controller('ReceiptInfoModalCtrl',['$scope','$uibModalInstance', '$location','itemData','PackageItem', function ($scope, $uibModalInstance, $location,itemData,PackageItem) {
    $scope.transactions = itemData.transaction_id;
    $scope.receipt = itemData;
+   $scope.pacName = [];
    console.log($scope.transactions);
+
+   for (var i = 0; i < $scope.transactions.length; i++) {
+     PackageItem.query({id : $scope.transactions[i].packages_id[0]}).$promise.then(function(res){
+      $scope.pacName.push(res.name);
+    });
+   };
+
    $scope.closeModal = function(){
       $uibModalInstance.close();
   };
+
+
   // get date
   $scope.getCreateDate = function(timeStamp){
     $scope.temp = new Date(timeStamp);
     $scope.createDate = $scope.temp.getDate() + "/" + ($scope.temp.getMonth() + 1) + "/" + $scope.temp.getFullYear();
     return $scope.createDate;
-  }
+  };
+
   // $scope.openEditPage = function(){
   // 	$location.path('/edit-package');
   // 	$uibModalInstance.close();
