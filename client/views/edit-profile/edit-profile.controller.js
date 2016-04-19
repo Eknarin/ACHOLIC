4c8c11137509152358c9e882a3e0a1662307abfd
Natcha119 <natchachid@gmail.com>
2016-04-19 15:56:06 +0700

    Fix profile bug.

--- client/views/edit-profile/edit-profile.controller.js
+++ client/views/edit-profile/edit-profile.controller.js
@@ -3,24 +3,25 @@
 angular.module('acholic')
 .controller('EditProfileCtrl',[ '$scope','Auth','User','$location', function ($scope, Auth,User,$location) {
 	Auth.getUser().then(function(res){
-		User.query_user({id:res._id}).$promise.then(function(user){
-			$scope.user_edit = user;
+		// User.query_user({id:res._id}).$promise.then(function(user){
+			$scope.user_edit = res;
   			  	//Check BD
+  			//  console.log($scope.user_edit);
 
   			  	if($scope.user_edit.date_of_birth)
   			  		$scope.user_edit.date_of_birth = $scope.user_edit.date_of_birth.substring(0, 10);
 
-			  	//Check gender from F acebook
-			  	if($scope.user_edit.gender === 'female' || $scope.user_edit.gender === 'Women'){
-			  		$scope.user_edit.gender = 'Women'
-			  	}
-			  	else if($scope.user_edit.gender === 'male' || $scope.user_edit.gender === 'Men'){
-			  		$scope.user_edit.gender = 'Men'
-			  	}
-			  	else{
-			  		$scope.user_edit.gender = 'Other'
-			  	}
-			  });
+			  // 	//Check gender from F acebook
+			  // 	if($scope.user_edit.gender === 'female' || $scope.user_edit.gender === 'Women'){
+			  // 		$scope.user_edit.gender = 'Women'
+			  // 	}
+			  // 	else if($scope.user_edit.gender === 'male' || $scope.user_edit.gender === 'Men'){
+			  // 		$scope.user_edit.gender = 'Men'
+			  // 	}
+			  // 	else{
+			  // 		$scope.user_edit.gender = 'Other'
+			  // 	}
+			  // });
 	});
 
 	$scope.tabs = ["active", "", "", ""];
@@ -31,46 +32,45 @@ angular.module('acholic')
 
 	$scope.onSubmit= function(){
 		if($scope.user_edit.vendor){
-			console.log("I'm the vendor");
 		      //check id
 		      if($scope.user_edit.vendor.identification_num == null){
-		      	alert("Please enter your Identification Number.");
+		      	alert("กรุณากรอกเลขที่บัตรประชาชน");
 		      	return;
 		      }
 		      //check travel issue 
 		      if($scope.user_edit.vendor.provider_num == null){
-		      	alert("Please enter your Tourist Business license no.");
+		      	alert("กรุณากรอกเลขที่");
 		      	return;
 		      }
 		      //check email
 		      if($scope.user_edit.vendor.alt_email == null){
-		      	alert("Please enter Vendor Email.");
+		      	alert("กรุณากรอกอีเมลของผู้ค้า");
 		      	return;
 		      }
 		      //check phone
 		      if($scope.user_edit.vendor.phone_number == null){
-		      	alert("Please enter Vendor Phone Number.");
+		      	alert("กรุณากรอกหมายเชขโทรศัพท์ของผู้ค้า");
 		      	return;
 		      }
 		  }
 		  //check first name
 		  if($scope.user_edit.first_name == null){
-		  	alert("Please enter your First Name.");
+		  	alert("กรุณากรอกชื่อจริง");
 		  	return;
 		  }
 	      //check last name
 	      if($scope.user_edit.last_name == null){
-	      	alert("Please enter your Last Name.");
+	      	alert("กรุณากรอกนามสกุล");
 	      	return;
 	      }
 	      //check email
 	      if($scope.user_edit.email == null){
-	      	alert("Please fill your Email.");
+	      	alert("กรุณากรอกอีเมลของคุณ");
 	      	return;
 	      }
 	      //check phone number
 	      if($scope.user_edit.phone_number == null){
-	      	alert("Please fill your Phone number.");
+	      	alert("กรุณากรอกหมายเลขโทรศัพท์ของคุณ");
 	      	return;
 	      }
 
