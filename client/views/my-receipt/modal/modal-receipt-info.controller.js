c6fbfa04cbb4f91828247e2b9ef185ed3c0e4c4e
Kridpak <s.vorametpasuk@gmail.com>
2016-04-19 14:01:44 +0700

    ADD show transaction status

--- client/views/my-receipt/modal/modal-receipt-info.controller.js
+++ client/views/my-receipt/modal/modal-receipt-info.controller.js
@@ -5,7 +5,7 @@ angular.module('acholic')
    $scope.transactions = itemData.transaction_id;
    $scope.receipt = itemData;
    $scope.pacName = [];
-
+   
    for (var i = 0; i < $scope.transactions.length; i++) {
      PackageItem.query({id : $scope.transactions[i].packages_id[0]}).$promise.then(function(res){
       $scope.pacName.push(res.name);
