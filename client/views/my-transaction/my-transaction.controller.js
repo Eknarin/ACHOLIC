5f624fa5d92736546b928a1148f7c1e210687136
Kridpak <s.vorametpasuk@gmail.com>
2016-04-19 21:01:55 +0700

    Add my transaction to my transaction

--- client/views/my-transaction/my-transaction.controller.js
+++ client/views/my-transaction/my-transaction.controller.js
@@ -28,6 +28,54 @@ angular.module('acholic')
         });
    });
 
+    $scope.choosePack = function(packageId){
+      if(packageId == 0){
+        $scope.choosen = false;
+        Transaction.queryTran({vendor_id: $scope.user._id,page:1}).$promise.then(function(res){
+              $scope.receipts = res.docs;
+              $scope.limit = res.limit;
+              $scope.totalItems = res.total;
+          });
+      }else{
+        $scope.choosen = true;
+        $scope.currentPack = packageId;
+       Transaction.queryPack({vendor_id: $scope.user._id,page:1,package_id:packageId}).$promise.then(function(res){
+              $scope.receipts = res.docs;
+              $scope.limit = res.limit;
+              $scope.totalItems = res.total;              
+       });
+     }
+    };
+
+        $scope.getPackge = function(package_id){
+      for(var i = 0;i<$scope.packages.length ;i++){
+        if($scope.packages[i]._id == package_id)
+        {
+          return $scope.packages[i];
+        }
+      }
+    };
+
+        $scope.pageChanged = function() {
+      if($scope.choosen == true){
+        Transaction.queryPack({vendor_id: $scope.user._id,page:$scope.currentPage,package_id:$scope.currentPack}).$promise.then(function(res){
+              $scope.receipts = res.docs;
+              $scope.limit = res.limit;
+              $scope.totalItems = res.total;
+        });
+      }
+      else{
+         Transaction.queryTran({vendor_id: $scope.user._id,page:$scope.currentPage}).$promise.then(function(res){
+            $scope.receipts = res.docs;
+            $scope.limit = res.limit;
+            $scope.totalItems = res.total;
+          });
+       }
+    };
+
+
+
+
     $scope.checkSerial = function(){
       Transaction.serial({'serial':$scope.serial}).$promise.then(function(serial){
         if(serial._id)
